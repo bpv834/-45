37c2092388e6918fed58dc742b7e834f6af55ec8
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-05-29 19:56:43 +0900

    초기 커밋.

---
+++ 동적 메모리를 이용한 정수의 합계 구하기/동적 메모리를 이용한 정수의 합계 구하기.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int size;
+	int* arr;
+	arr = NULL;
+
+	int i;
+	int sum = 0;
+
+	printf("정수의 개수:");
+	scanf_s("%d", &size);
+
+	arr = (int*)(malloc(sizeof(int) * size));
+
+
+	printf("%d개의 정수를 입력하세요", size);
+
+	for (i = 0; i < size; i++)
+		scanf_s("%d", &arr[i]);
+	for (i = 0; i < size; i++) 
+		sum += arr[i];
+	
+	printf("입력된 정수의 합계:%d", sum);
+	free(arr);
+	arr = NULL;
+
+} 
+
